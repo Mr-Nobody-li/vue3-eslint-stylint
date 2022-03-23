c9cbb674117e0a30abb520656151b91049675a79
李鹏飞 <pengfei.li@xbongbong.com>
2022-03-23 10:47:28 +0800

    feat: 配置eslint stylint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    // 允许非空断言
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    // 允许自定义模块和命名空间
+    '@typescript-eslint/no-namespace': 'off',
+    // 允许对this设置alias
+    '@typescript-eslint/no-this-alias': 'off',
+    // 允许使用any类型
+    '@typescript-eslint/no-explicit-any': ['off']
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,21 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 nav {
+  width: 30px;
+  width: 0%;
   padding: 30px;
 
   a {
